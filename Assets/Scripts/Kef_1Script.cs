de5fb708b03cad706cb1924ed6f731691bc8e9c3
NikolaosProgios <nikosprogios@gmail.com>
2022-11-27 19:51:01 +0200

    New Commit!

--- Assets/Scripts/Kef_1Script.cs
+++ Assets/Scripts/Kef_1Script.cs
@@ -8,54 +8,70 @@ using UnityEngine.UI;
 public class Kef_1Script : MonoBehaviour
 {
     public TMP_Text TableQuestion;
-    public TMP_Text Answer1Text;
-    public TMP_Text Answer2Text;
-    public TMP_Text Answer3Text;
-    int score;
-    int i, k, j;
+    public GameObject Answer1btn; public TMP_Text Answer1Text;
+    public GameObject Answer2btn; public TMP_Text Answer2Text;
+    public GameObject Answer3btn; public TMP_Text Answer3Text; 
+    int correctAnsw=0;
+    int line=0, row=0, column=0;
 
     string[] Questions = {
         "Ποιες ομοιότητες παρατηρείτε ανάμεσα στην Αμερικανική "+
             "και την Γαλλική Επανάσταση;",
         "Ποια θεωρούνται τα σημαντικότερα ανθρώπινα δικαιώματα με βάση "+
-                "τα κείμενα των δύο πηγών; Τι πιστεύουμε σήμερα;" +
+                "τα κείμενα των δύο πηγών; Τι πιστεύουμε σήμερα;" ,
         "Ετσι με αρέσει"
     };
-    string[,] Answers = { { "1Anse1 ", "1Anse2 ", "1Anse3" },
-                          { "2Anse1 ", "2Anse2 ", "3Anse3" },
+    string[,] Choices = { { "1Anse1 ", "1Anse2 ", "1Anse3" },
+                          { "2Anse1 ", "2Anse2 ", "2Anse3" },
                           { "3Anse1 ", "3Anse2 ", "3Anse3" }
     };
 
+    int[] correctAnswers = {1,2,2};
+
     void Start()
-    {
-        i = 0; k = 0; j = 0;
-        TableQuestion.text = Questions[i++].ToString();
-        Answer1Text.text = Answers[k, j++].ToString();
-        Answer2Text.text = Answers[k, j++].ToString();
-        Answer3Text.text = Answers[k++, j++].ToString();
-        j = 0;
+    { Debug.Log("Start()");
+        
+        TableQuestion.text = Questions[line++].ToString();
+        Answer1Text.text = Choices[row, column++].ToString();
+        Answer2Text.text = Choices[row, column++].ToString();
+        Answer3Text.text = Choices[row++, column].ToString(); column = 0;
     }
-    private bool LoadNextQ()
-    {
-        bool endKef = false;
-        if ((i < Questions.Length) & (k < Answers.Length))
-        {
-            TableQuestion.text = Questions[i++].ToString();
-            Answer1Text.text = Answers[k, j++].ToString();
-            Answer2Text.text = Answers[k, j++].ToString();
-            Answer3Text.text = Answers[k++, j++].ToString(); j = 0;
+
+    public void PressedAnswer(int choice)
+    { Debug.Log("PressedAnswer()");
+
+        if (choice == correctAnswers[--line]) {
+            correctAnsw++; line++;
         }
-        else
+        if (LoadNextQnA())
         {
+            TableQuestion.text = "Τέλος 1ης Ενότητας."
+                + "\nΣωστες Απαντήσεις:" + correctAnsw
+                + "\nΛανθασμένες Απαντήσεις:" + (Questions.Length-correctAnsw);
+            Answer1btn.SetActive(false);
+            Answer2btn.SetActive(false);
+            Answer3btn.SetActive(false);
+        }
+    }
+
+    private bool LoadNextQnA()
+    { Debug.Log("LoadNextQnA()");
+
+        bool endKef = false;
+        if ((line < Questions.Length) & (row < Choices.Length))
+        {
+            TableQuestion.text = Questions[line++].ToString();
+            Answer1Text.text = Choices[row, column++].ToString();
+            Answer2Text.text = Choices[row, column++].ToString();
+            Answer3Text.text = Choices[row++, column].ToString(); column = 0;       
+        }
+        else {
             endKef = true;
         }
         return endKef;
     }
-    void Update() { }
 
-
-    public void Piso()
-    {
+    public void Piso() {
         SceneManager.LoadScene("MenuScene");
     }
 }
