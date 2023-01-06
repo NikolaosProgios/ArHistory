200284e7bbf5ae4d7a10b07a33ad56d73f574e00
NikolaosProgios <nikosprogios@gmail.com>
2023-01-06 18:47:08 +0200

    v0.3.5

--- Game_Project/Assets/Scripts/Kef_5Script.cs
+++ Game_Project/Assets/Scripts/Kef_5Script.cs
@@ -1,24 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine.UI;
+using System.Threading.Tasks;
 
 public class Kef_5Script : MonoBehaviour
 {
-    private TextMeshPro textMesH;/*
-    string Q5A = "Ποιοι είναι οι κυριότεροι σταθμοί στην πορεία προς την ευρωπαϊκή ενοποίηση;";
-    string Q5B = "Ποιο ήταν το όραμα του Κωνσταντίνου Καραμανλή για την Ελλάδα; Θα σας βοηθήσει η Πηγή 1.";
-    */
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject Welcome_Panel, WelcomeImage, GoodByeΙmage,
+    StarKef5Game, EndKef5NextGame;
+    public TMP_Text TitlePanel;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public GameObject AnswersCanvas;
+    public TMP_Text TableQuestion,
+            Answer1Text, Answer2Text, Answer3Text;
+    public Button Answer1btn, Answer2btn, Answer3btn;
+    int line, row_txt, column, row_img, correctAnswersCounter;
+
+    string[] Questions = {
+        "Ποιοι είναι οι κυριότεροι σταθμοί στην πορεία προς την ευρωπαϊκή ενοποίηση; "
+        ,"Ποιο ήταν το όραμα του Κωνσταντίνου Καραμανλή για την Ελλάδα; Θα σας βοηθήσει η Πηγή 1."
+        ,"Στην μάχες που σηνέυησαν παλαιότερα στην κωνσταντινούπολη (14), μικρα ασία, ποντος, ίμια, κυπρος"+
+         "ποιος είναι ο κοινός εχθρος;"
+        ,"Photo Answers1","Photo Answers2","Photo Answers3",
+    };
+
+    string[,] Choices = { { "1Anse1", "1Anse2", "1Anse3" },
+                          { "Κίνα", "Τουρκία", "Αλβανία" },
+                          { "3Anse1", "3Anse2", "3Anse3" }
+    };
+    int[] correctAnswers = { 1, 2, 2, 1, 2, 2 };
+
+
 }
